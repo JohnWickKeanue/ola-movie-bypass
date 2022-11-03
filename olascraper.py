aa2735cad6fe32d4cfced9bdf730a81e9ed91141
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-03 12:02:35 +0530

    Update olascraper.py

--- olascraper.py
+++ olascraper.py
@@ -97,9 +97,7 @@ def olamovies(url):
         while 'rocklinks.net' not in soup and "try2link.com" not in soup:
          
             res = client.get("https://olamovies.ink/download/", params=params, headers=headers)
-            soup = BeautifulSoup(res.text,"html.parser")
-           
-            jack = soup.text
+            jack = res.text
             rose = jack.split('url = "')[-1]
             soup = rose.split('";')[0]
             
